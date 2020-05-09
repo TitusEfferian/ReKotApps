b37fcc19e80d28e83f01a37e5c626388ed593e61
titus efferian <titus.efferian@tokopedia.com>
2020-05-09 11:21:57 +0700

    Spinner

--- Navigations/Register/components/Button.js
+++ Navigations/Register/components/Button.js
@@ -1,6 +1,6 @@
 import React, {memo} from 'react';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Spinner from '../../../components/Spinner';
 
 const Button = () => {
   return (
