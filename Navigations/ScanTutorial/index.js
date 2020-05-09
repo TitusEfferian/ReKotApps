88dc3ae9fd7803ae2e4cd789cb77479dd6cbe7e2
titus efferian <titus.efferian@tokopedia.com>
2020-05-09 14:16:03 +0700

    scan tutorial

--- Navigations/ScanTutorial/index.js
+++ Navigations/ScanTutorial/index.js
@@ -1,8 +1,37 @@
 import React, {memo} from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import Logo from './components/Logo';
 
 const ScanTutorial = () => {
-  return <Text>scan tutorial</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        <Logo />
+        <Text style={styles.textStyle}>
+          Scan QR code yang terdapat dalam stiker KIR. Stiker KIR dapat
+          ditemukan di pintu angkot.
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#B32727',
+  },
+  scrollViewContainer: {
+    alignItems: 'center',
+    paddingHorizontal: 60,
+  },
+  textStyle: {
+    fontSize: 16,
+    lineHeight: 20,
+    color: 'white',
+    textAlign: 'center',
+    marginTop: 24,
+  },
+});
 
 export default memo(ScanTutorial);
